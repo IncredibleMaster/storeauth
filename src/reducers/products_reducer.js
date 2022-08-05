74649da1ed42e5944d1f47d4159d64079bd160e7
IncredibleMaster <golden.forest129@gmail.com>
2022-08-05 15:00:03 +0300

    feat: add reducer function used with useReducer

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -10,8 +10,17 @@ import {
 } from '../actions'
 
 const products_reducer = (state, action) => {
+if(action.type === SIDEBAR_OPEN){
+  return (
+    {...state,isSidebarOpen: true}
+  )
+}
+if (action.type === SIDEBAR_CLOSE) {
+  return { ...state, isSidebarOpen: false };
+}
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
 export default products_reducer
+
