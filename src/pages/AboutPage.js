8ac137b83017747fa083ccd069e92f74f5b86dfe
IncredibleMaster <golden.forest129@gmail.com>
2022-08-06 11:49:23 +0300

    feat: add page content

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about'/>
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='awesome desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, asperiores similique voluptas molestiae eius nemo odio iusto, accusamus beatae ipsa optio dolor in id iste. Voluptas dolorum possimus doloremque alias voluptatibus! Fugit, cum magnam et numquam sapiente consequatur temporibus explicabo quia voluptates illo at aliquid quis esse tempora laborum. Dignissimos, aut animi! Quidem nesciunt distinctio vitae eaque, maiores eveniet voluptatem explicabo deserunt adipisci odit saepe.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +49,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
