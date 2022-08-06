4da9495d638b6f8e7a8a08802238ed7a991249fc
IncredibleMaster <golden.forest129@gmail.com>
2022-08-06 11:51:01 +0300

    feat: add content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit
+            consequatur provident, sint quod sapiente maxime reprehenderit
+            reiciendis dicta autem!
+          </p>
+          <form
+            className='contact-form'
+            action='https://formspree.io/f/xeqdjdda'
+            method='POST'
+          >
+            <input
+              type='email'
+              name='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
