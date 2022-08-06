e5f03eece05461bf71c53826103da9b3b34d5285
IncredibleMaster <golden.forest129@gmail.com>
2022-08-06 11:50:33 +0300

    feat: add content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,28 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+    <Wrapper className='section-center'>
+      <article className='content'>
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+          inventore, cumque, iure accusamus possimus temporibus illo at
+          voluptatem minima iusto pariatur. Facilis, magnam?
+        </p>
+        <Link to='/products' className='btn hero-btn'>
+          shop now
+        </Link>
+      </article>
+      <article className='img-container'>
+        <img src={heroBcg} alt='awesome table' className='main-img' />
+        <img src={heroBcg2} alt='carpenter busy' className='accent-img' />
+      </article>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.section`
