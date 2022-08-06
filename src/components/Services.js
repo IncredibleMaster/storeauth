8ec3df3d69c5f615e85d866ce51b067a511ae87f
IncredibleMaster <golden.forest129@gmail.com>
2022-08-06 11:50:47 +0300

    feat: add content

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,31 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return <Wrapper>
+    <div className="section-center">
+      <article className="header">
+        <h3>
+          custom furniture <br />
+          built only for you 
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae asperiores eius voluptatibus porro, esse magnam voluptas, ducimus voluptatum sint quam debitis ratione harum.
+        </p>
+      </article>
+      <div className="services-center">
+        {services.map((service)=> {
+          const{id,icon,title,text}=service
+          return (
+            <article key={id} className="service">
+              <span className="icon">{icon}</span>
+              <h4>{title}</h4>
+              <p>{text}</p>
+            </article>
+          )
+        })}
+      </div>
+    </div>
+     </Wrapper>
 }
 
 const Wrapper = styled.section`
