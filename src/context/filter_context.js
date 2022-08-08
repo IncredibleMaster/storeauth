e7b3b58aa1690628eae73c0dd8e4dc262d8ab3e0
IncredibleMaster <golden.forest129@gmail.com>
2022-08-08 17:45:27 +0300

    add state management for the context

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/filter_reducer";
 import {
   LOAD_PRODUCTS,
   SET_GRIDVIEW,
@@ -9,21 +9,39 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
-import { useProductsContext } from './products_context'
+} from "../actions";
+import { useProductsContext } from "./products_context";
 
-const initialState = {}
+const initialState = {
+  all_products: [],
+  filtered_products: [],
+  grid_view:true,
 
-const FilterContext = React.createContext()
+};
+
+const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
+  const setGridView = ()=> {
+    dispatch({type: SET_GRIDVIEW})
+  }
+  const setListView = ()=> {
+    dispatch({type: SET_LISTVIEW})
+  }
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{ ...state,setListView, setGridView }}>
       {children}
     </FilterContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
-}
+  return useContext(FilterContext);
+};
