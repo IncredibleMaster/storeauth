2819fd7f8e3dc54d9527ed004f5896b8db5180a8
IncredibleMaster <golden.forest129@gmail.com>
2022-08-10 07:22:46 +0300

    feat: get unique value

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,4 +6,12 @@ export const formatPrice = (number) => {
   return newNumber;
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data,type) => {
+  let unique = data.map((item)=> item[type])
+
+  if(type === 'colors'){
+    unique = unique.flat()
+  }
+  return ['all',...new Set(unique)]
+
+};
